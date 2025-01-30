3ec832434e1f59131cb98872e66fad4dbdd51ca9
dineth panditha <92966321+dinethh@users.noreply.github.com>
2025-01-30 13:08:24 +0530

    email part is done

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -1,3 +1,4 @@
+
 const nodemailer = require('nodemailer');
 
 const sendEmail = async (to, subject, text) => {
@@ -5,7 +6,7 @@ const sendEmail = async (to, subject, text) => {
         const transporter = nodemailer.createTransport({
             host: process.env.EMAIL_HOST,
             port: process.env.EMAIL_PORT,
-            secure: false, // true for 465, false for other ports
+            secure: false,
             auth: {
                 user: process.env.EMAIL_USER,
                 pass: process.env.EMAIL_PASS
@@ -27,3 +28,4 @@ const sendEmail = async (to, subject, text) => {
 };
 
 module.exports = sendEmail;
+
